f9caa1fdb7cc0c3e11624a7ec8495162a9667c3b
정민규 <jmg081005@naver.com>
2024-01-26 19:34:48 +0900

    Update 섬연결하기.js

--- 정민규/프로그래머스_3단계/섬연결하기.js
+++ 정민규/프로그래머스_3단계/섬연결하기.js
@@ -1,3 +1,4 @@
+//union-find
 const getParent = (a, arr) => {
   if (arr[a] === a) return a;
   return (arr[a] = getParent(arr[a], arr));
@@ -23,14 +24,17 @@ const checkIsConnect = (arr) => {
 };
 
 function solution(n, costs) {
+  //가격낮은순으로 정렬
   costs.sort((a, b) => a[2] - b[2]);
   let parents = [...Array(n)].map((_, i) => i);
 
   let ret = 0;
   for (const [s, e, c] of costs) {
+    // 연결이 안되어있다면 연결해줌
     if (!isUnion(s, e, parents)) {
       union(s, e, parents);
       ret += c;
+      //연결한 후에 모든 섬이 연결되었는지 확인
       parents = parents.map((v) => getParent(v, parents));
       if (checkIsConnect(parents)) return ret;
     }
